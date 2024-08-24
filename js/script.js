ffa4367f752bb20a4c5e512b755c391f0fbac916
eyoba146 <eyobadamu146@gmail.com>
2024-08-24 16:04:12 -0300

    add maxlength with js on cvc input

--- js/script.js
+++ js/script.js
@@ -71,7 +71,10 @@ year.addEventListener("input",function (){
     updatedDateFormat = ""
 
 })
-cvc.addEventListener("input",()=> {
+cvc.addEventListener("input",function() {
+    if (this.value.length > this.maxLength) {
+        this.value = this.value.slice(0, this.maxLength);
+    }
     myCvc.innerText = cvc.value
 })
 
